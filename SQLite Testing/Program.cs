658fbd58202583fcf913e8e0ab121cc1c27195fa
Chaim Straus <chaim.straus@gmail.com>
2021-10-20 21:11:25 -0400

    so I fixed the completely stupid thing where it repoened the sql file every single time you wanted to CRUD...

--- SQLite Testing/Program.cs
+++ SQLite Testing/Program.cs
@@ -6,34 +6,28 @@ namespace SQLite_Testing
 {
     class Program
     {
-        public static void CRUDwithouttheR(string database, string SQLcommand)
+        public static string database = "database.db";
+        public static SQLiteConnection connection = new SQLiteConnection($"Data Source = {database}");
+
+        public static void CRUDwithouttheR(string SQLcommand)
         {
-            using (var connection = new SQLiteConnection($"Data Source = {database}"))
+            using var command = new SQLiteCommand(connection);
             {
-                connection.Open();
-                using var command = new SQLiteCommand(connection);
-                {
-                    command.CommandText = SQLcommand;
-                    command.ExecuteNonQuery();
-                }
+                command.CommandText = SQLcommand;
+                command.ExecuteNonQuery();
             }
         }
 
-        public static Array CRUDwithouttheCUD(string database, string SQLcommand)
+        public static Array CRUDwithouttheCUD(string SQLcommand)
         {
             List<Array> data = new List<Array>();
-            using (var connection = new SQLiteConnection($"Data Source = {database}"))
+            using (var command = new SQLiteCommand(SQLcommand, connection))
+            using (SQLiteDataReader rdr = command.ExecuteReader())
             {
-                connection.Open();
-                using (var command = new SQLiteCommand(SQLcommand, connection))
-                using (SQLiteDataReader rdr = command.ExecuteReader())
+                while (rdr.Read())
                 {
-                    while (rdr.Read())
-                    {
-                        // Console.WriteLine($"{rdr.GetInt32(0)} | {rdr.GetString(1)} | {rdr.GetInt32(2)}");
-                        object[] row = { rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2)};
-                        data.Add(row);
-                    }
+                    object[] row = { rdr.GetInt32(0), rdr.GetString(1), rdr.GetInt32(2)};
+                    data.Add(row);
                 }
             }
             return data.ToArray();
@@ -41,35 +35,45 @@ namespace SQLite_Testing
 
         static void Main(string[] args)
         {
-            string db = "database.db";
-            CRUDwithouttheR(db, "DROP TABLE testTable");
-            Console.WriteLine("Table dropped.");
-            CRUDwithouttheR(db, "CREATE TABLE IF NOT EXISTS testTable (id integer primary key autoincrement, name text, balance integer)");
-            Console.WriteLine("Table initialized.");
-            Console.WriteLine("Inserting data...");
-            CRUDwithouttheR(db, "INSERT INTO testTable (name, balance) VALUES ('chaim', 100)");
-            CRUDwithouttheR(db, "INSERT INTO testTable (name, balance) VALUES ('michael', 90)");
-
-            Console.WriteLine("Insert some of your own data!");
-
-            Console.WriteLine("What name would you like to insert?");
-            string name = Console.ReadLine();
-
-            Console.WriteLine($"What balance would you like to associate with {name}?");
-            int balance = int.Parse(Console.ReadLine());
-
-            CRUDwithouttheR(db, $"INSERT INTO testTable (name, balance) VALUES ('{name}', {balance})");
-
-            Array data = CRUDwithouttheCUD(db, "SELECT * FROM testTable");
-            foreach (object[] datapoint in data)
+            using (connection)
             {
-                foreach (object item in datapoint)
-                {
-                    Console.Write($"{item} ");
+                connection.Open();
+
+                CRUDwithouttheR("DROP TABLE testTable");
+                Console.WriteLine("Table dropped.");
+                CRUDwithouttheR("CREATE TABLE IF NOT EXISTS testTable (id integer primary key autoincrement, name text, balance integer)");
+                Console.WriteLine("Table initialized.");
+                Console.WriteLine("Inserting data...");
+                CRUDwithouttheR("INSERT INTO testTable (name, balance) VALUES ('chaim', 100)");
+                CRUDwithouttheR("INSERT INTO testTable (name, balance) VALUES ('michael', 90)");
+
+                Console.WriteLine("Insert some of your own data!");
+
+                Console.WriteLine("What name would you like to insert?");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"What balance would you like to associate with {name}?");
+
+                int balance;
+                bool converted = int.TryParse(Console.ReadLine(), out balance);
+                if (!(converted)) {
+                    Console.WriteLine("That is not an integer, and I do NOT want to deal with this right now. \nCancelling run.");
+                    return;
                 }
-                Console.WriteLine();
-                //string column = string.Concat(datapoint);
-                //Console.WriteLine(column);
+
+                CRUDwithouttheR($"INSERT INTO testTable (name, balance) VALUES ('{name}', {balance})");
+
+                Array data = CRUDwithouttheCUD("SELECT * FROM testTable");
+                foreach (object[] datapoint in data)
+                {
+                    foreach (object item in datapoint)
+                    {
+                        Console.Write($"{item} ");
+                    }
+                    Console.WriteLine();
+                    //string column = string.Concat(datapoint);
+                    //Console.WriteLine(column);
+                }
             }
         }
     }
