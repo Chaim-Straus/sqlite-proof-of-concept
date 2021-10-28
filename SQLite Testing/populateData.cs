1e43614248e142eae7bb37981c098f023e0a2d67
Chaim <chaim.straus@gmail.com>
2021-10-28 16:22:10 -0400

    updated passwords

--- SQLite Testing/populateData.cs
+++ SQLite Testing/populateData.cs
@@ -6,9 +6,9 @@
         {
             main.CreateUpdateOrDelete("DROP TABLE testTable");
             main.CreateUpdateOrDelete("CREATE TABLE IF NOT EXISTS testTable (id integer primary key autoincrement, name text, password text, balance integer)");
-            main.CreateUpdateOrDelete($"INSERT INTO testTable (name, password, balance) VALUES ('Chaim Straus', '{encryptSHA256.encrypt("f4d3s2a1")}', 100000)");
-            main.CreateUpdateOrDelete($"INSERT INTO testTable (name, password, balance) VALUES ('Michael Roberts', '{encryptSHA256.encrypt("a1s2d3f4")}', 99999)");
-            main.CreateUpdateOrDelete($"INSERT INTO testTable (name, password, balance) VALUES ('Shalom Feuer', '{encryptSHA256.encrypt("pityaccount")}', 99998)");
+            main.CreateUpdateOrDelete($"INSERT INTO testTable (name, password, balance) VALUES ('Chaim Straus', '{encryptSHA256.encrypt("chaim")}', 100000)");
+            main.CreateUpdateOrDelete($"INSERT INTO testTable (name, password, balance) VALUES ('Michael Roberts', '{encryptSHA256.encrypt("michael")}', 99999)");
+            main.CreateUpdateOrDelete($"INSERT INTO testTable (name, password, balance) VALUES ('Shalom Feuer', '{encryptSHA256.encrypt("shalom")}', 99998)");
         }
     }
 }
